c3230b499d1263866c07d45fec37f4a9636bd9c8
Michael Frieze <michaelfrieze@icloud.com>
2021-12-25 14:57:36 -0500

    installed and setup tailwind & postcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './app/**/*.tsx',
+    './app/**/*.jsx',
+    './app/**/*.js',
+    './app/**/*.ts',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
