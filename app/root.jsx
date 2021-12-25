7579959454f2e9f5a4e258a3d7003b368279d218
Michael Frieze <michaelfrieze@icloud.com>
2021-12-25 16:13:30 -0500

    setup root

--- app/root.jsx
+++ app/root.jsx
@@ -1,4 +1,12 @@
-import { Links, LiveReload } from 'remix';
+import {
+  Links,
+  LiveReload,
+  Outlet,
+  Meta,
+  ScrollRestoration,
+  Scripts,
+  useCatch,
+} from 'remix';
 
 import globalStylesUrl from './styles/globals.css';
 import tailwindStylesUrl from './styles/tailwind.css';
@@ -9,20 +17,108 @@ export const links = () => {
       rel: 'stylesheet',
       href: globalStylesUrl,
     },
+    {
+      rel: 'stylesheet',
+      href: tailwindStylesUrl,
+    },
   ];
 };
 
-export default function App() {
+export const meta = () => {
+  const description = `Blogging about web development, design, and life.`;
+  return {
+    description,
+    keywords: 'devspace blog',
+  };
+};
+
+function Document({ children, title = `DevSpace Blog` }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <title>Remix: So great, it's funny!</title>
+        <title>{title}</title>
+        <Meta />
+        <Links />
       </head>
       <body>
-        Hello world
+        {children}
+        <ScrollRestoration />
+        <Scripts />
         {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
     </html>
+  );
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <main className="min-h-screen -mb-16">{children}</main>
+    </>
+  );
+}
+
+export function ErrorBoundary({ error }) {
+  console.error(error);
+  return (
+    <Document title="Error!">
+      <Layout>
+        <div>
+          <h1>There was an error</h1>
+          <p>{error.message}</p>
+          <hr />
+          <p>
+            Hey, developer, you should replace this with what you want your
+            users to see.
+          </p>
+        </div>
+      </Layout>
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  let caught = useCatch();
+
+  let message;
+  switch (caught.status) {
+    case 401:
+      message = (
+        <p>
+          Oops! Looks like you tried to visit a page that you do not have access
+          to.
+        </p>
+      );
+      break;
+    case 404:
+      message = (
+        <p>Oops! Looks like you tried to visit a page that does not exist.</p>
+      );
+      break;
+
+    default:
+      throw new Error(caught.data || caught.statusText);
+  }
+
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <Layout>
+        <h1>
+          {caught.status}: {caught.statusText}
+        </h1>
+        {message}
+      </Layout>
+    </Document>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
   );
 }
