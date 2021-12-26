b88601d010dc0217fc7111e880f4aa970066a654
Michael Frieze <michaelfrieze@icloud.com>
2021-12-26 16:11:54 -0500

    separated the get posts function from index and put it in library directory | created a util directory with a module that sorts by date

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,30 +1,4 @@
 import { useLoaderData, Link } from 'remix';
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-
-export const loader = () => {
-  const files = fs.readdirSync(path.join('posts'));
-
-  const posts = files.map((filename) => {
-    const slug = filename.replace('.md', '');
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', filename),
-      'utf8'
-    );
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
-
-  console.log(posts);
-  return posts;
-};
 
 export const meta = () => {
   return {
