d4ac96adbf7494f955a9b30c9568c69328861dad
Michael Frieze <michaelfrieze@icloud.com>
2021-12-26 15:24:51 -0500

    fixed error | there was an anchor tag inside of Link element and remix did not like it

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -4,22 +4,25 @@ export default function Header() {
   return (
     <header className="bg-gray-900 text-gray-100 shadow w-full">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <Link to="/">
-          <a className="flex md:w-1/5 title-font font-medium items-center md:justify-start mb-4 md:mb-0">
-            <img src="/images/logo.png" alt="logo" width="100" height="100" />
-            <span className="ml-3 text-xl">DevSpace</span>
-          </a>
+        <Link
+          to="/"
+          className="flex md:w-1/5 title-font font-medium items-center md:justify-start mb-4 md:mb-0"
+        >
+          <img src="/images/logo.png" alt="logo" width="40" height="40" />
+          <span className="ml-3 text-xl">DevSpace</span>
         </Link>
         <nav className="flex flex-wrap md:w-4/5 items-center justify-end text-base md:ml-auto">
-          <Link to="/blog">
-            <a className="mx-5 cursor-pointer uppercase hover:text-indigo-300">
-              Blog
-            </a>
+          <Link
+            to="/blog"
+            className="mx-5 cursor-pointer uppercase hover:text-indigo-300"
+          >
+            Blog
           </Link>
-          <Link to="/about">
-            <a className="mx-5 cursor-pointer uppercase hover:text-indigo-300">
-              About
-            </a>
+          <Link
+            to="/about"
+            className="mx-5 cursor-pointer uppercase hover:text-indigo-300"
+          >
+            About
           </Link>
         </nav>
       </div>
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,18 @@
+// import { useLoaderData } from 'remix';
+
+// export const loader = () => {
+//   return [
+//     {
+//       slug: 'my-first-post',
+//       title: 'My First Post',
+//     },
+//     {
+//       slug: '90s-mixtape',
+//       title: 'A Mixtape I Made Just For You',
+//     },
+//   ];
+// };
+
 export const meta = () => {
   return {
     title: 'DevSpace Blog',
@@ -5,6 +20,8 @@ export const meta = () => {
 };
 
 export default function Index() {
+  // const posts = useLoaderData();
+  // console.log(posts);
   return (
     <>
       <h1>Hello World</h1>
