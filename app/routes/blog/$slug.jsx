f290609453e7c8b74b13006953c622f571cfb567
Michael Frieze <michaelfrieze@icloud.com>
2021-12-27 23:00:58 -0500

    dynamic blog route is now displaying the blog posta data | installed marked to parse markdown

--- app/routes/blog/$slug.jsx
+++ app/routes/blog/$slug.jsx
@@ -1,5 +1,7 @@
 import { Link, useLoaderData, useCatch, useParams } from 'remix';
 import { getPosts } from '../../lib/posts';
+import { marked } from 'marked';
+import CategoryLabel from '../../components/category-label';
 
 export const loader = async ({ params }) => {
   const posts = await getPosts();
@@ -32,10 +34,32 @@ export default function PostPage() {
   } = useLoaderData();
 
   return (
-    <div>
-      <h1>hello</h1>
-      <h1>{slug}</h1>
-    </div>
+    <>
+      <Link to="/blog">Go Back</Link>
+      <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+        <div className="flex justify-between items-center mt-4">
+          <h1 className="text-5xl mb-7">{title}</h1>
+          <CategoryLabel>{category}</CategoryLabel>
+        </div>
+        <img src={cover_image} alt="" className="w-full rounded" />
+
+        <div className="flex justify-between items-center bg-gray-100 p-2 my-8">
+          <div className="flex items-center">
+            <img
+              src={author_image}
+              alt=""
+              className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+            />
+            <h4>{author}</h4>
+          </div>
+          <div className="mr-4">{date}</div>
+        </div>
+
+        <div className="blog-text mt-2">
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+        </div>
+      </div>
+    </>
   );
 }
 
